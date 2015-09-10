9249ee2ddece4a0a785f4c794bfbd97afbb27b84
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-10 05:46:28 -0500

    Load middleware and rules

--- core/middleware.js
+++ core/middleware.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function (_instance, server) {
+module.exports = function (_instance, expressApplication) {
+    var API                 = require('boooljs-api')
+    ,   async               = require('async')
+    ,   middlewarePlugins   = API.Plugins.get('middleware');
+
+    var each = q.nbind(async.eachSeries, async);
+
+    return each(middlewarePlugins, function (plugin, next) {
+        try {
+            plugin.checkIntegrity();
+            if(plugin.route && plugin.method){
+                expressApplication[plugin.method](plugin.route, plugin.action);
+            } else if(plugin.route){
+                expressApplication.use(plugin.route, plugin.action);
+            } else {
+                expressApplication.use(plugin.action);
+            }
+        } catch(err){
+            next(err);
+        }
+    }).then(function () {
+        return expressApplication;
+    });
 
 };
