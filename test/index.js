55d1fd6627a2504b14ed73d9f5cd1323266586b6
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-15 22:19:57 -0500

    Fix tests to comply

--- test/index.js
+++ test/index.js
@@ -2,10 +2,14 @@
 'use strict';
 
 describe('Bool.js', function () {
-    var app;
+    var app
+    ,   resolver        = require('../lib/utils/resolve')
+    ,   booljs_express  = resolver('');
 
     before(function () {
-        app = require('bool.js')('com.example.api').setBase('example');
+        app = require('../../booljs')('com.example.api')
+            .setServerLoader(booljs_express)
+            .setBase('example');
     });
 
     it('Boots using express.js', function () { return app.run(); });
