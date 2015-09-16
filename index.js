928141a34c6e8b74eca316d0c4603b25d3ac0e8b
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-15 22:20:03 -0500

    Initializer

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var API = require('booljs-api');
-API.Plugins.register(require('./lib/middleware/cors'));
+var API         = require('booljs-api')
+,   resolver    = require('./lib/utils/resolve');
 
-module.exports = new API.ServerLoader('booljs-express', {
-    init: require('./lib/init'),
-    middleware: require('./lib/middleware'),
-    router: require('./lib/router'),
-    boot: require('./lib/boot')
-});
+module.exports = new API.ServerLoader(
+    'booljs-express', // Name
+    require('./lib/server'), // Functions
+    [ resolver('lib/middleware/cors') ] // Dependants
+);
