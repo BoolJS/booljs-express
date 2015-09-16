0bb392a3cd3f0988ac562f2546d347999bf0fbc5
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-15 20:26:49 -0500

    Apply API.ServerLoader plugin to library

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 var API = require('booljs-api');
 API.Plugins.register(require('./lib/middleware/cors'));
 
-module.exports = {
+module.exports = new API.ServerLoader('booljs-express', {
     init: require('./lib/init'),
     middleware: require('./lib/middleware'),
     router: require('./lib/router'),
     boot: require('./lib/boot')
-};
+});
