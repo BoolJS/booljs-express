772ecdff282603de99913995c2cdc7b6bc2da983
Pablo Andres Dorado Suarez <im@pandres95.me>
2016-05-10 06:17:39 -0500

    Update plugins to comply with new API definition.

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 'use strict';
 
-var API;
-try {
-    API = require('bool.js/api');
-} catch (err){
-    API = require('booljs-api');
-}
-var resolver    = require('./lib/utils/resolve');
+var API         = require('booljs-api')
+,   lib         = require('./lib/server')
+,   resolver    = require('./lib/utils/resolve');
 
-module.exports = new API.ServerLoader(
-    'booljs-express', // Name
-    require('./lib/server'), // Functions
-    [ resolver('lib/middleware/cors') ] // Dependants
-);
+module.exports = class Express extends API.ServerLoader{
+    constructor(){
+        super('booljs-express', [ resolver('lib/middleware/cors') ]);
+    }
+
+    init(_instance){
+        return lib.init(_instance);
+    }
+    middleware(_instance, router, middleware){
+        return lib.middleware(_instance, router, middleware);
+    }
+    preroute(_instance, expressApplication){
+        return lib.preroute(_instance, expressApplication);
+    }
+    route(_instance, router, middleware, route){
+        return lib.route(_instance, router, middleware, route);
+    }
+    postroute(_instance, expressApplication, router){
+        return lib.postroute(_instance, expressApplication, router);
+    }
+    boot(expressApplication){
+        return lib.boot(expressApplication);
+    }
+};
