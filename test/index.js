bd417fa21b0a6951bb3ac47192c935bef5121d4a
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-12-31 18:31:21 -0500

    Updated tests to assert default HTTP requests

--- test/index.js
+++ test/index.js
@@ -3,15 +3,23 @@
 
 const resolver = require('../lib/utils/resolve');
 const Bool = require('booljs');
+const Agent = require('supertest');
 
 describe('Bool.js', function () {
-    let app;
+    let app, server;
 
     before(() => {
         app = new Bool('com.example.api', [ resolver('') ])
             .setServerDrivers([ 'booljs.express' ])
             .setBase('example');
+        server = new Agent('http://localhost:8080');
     });
 
     it('Boots using express.js', () => app.run());
+
+    it('GET / -> 200 OK', () => server.get('/').expect(200));
+
+    it('GET /undefined -> 501 Not Implemented', () => server
+        .get('/undefined')
+        .expect(501));
 });
